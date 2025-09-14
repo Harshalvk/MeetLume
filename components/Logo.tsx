bac161694d1bbe98b3dc5f6cdfab47c880d32e6f
Harshalvk <harshalvkhobragade@gmail.com>
2025-09-14 21:28:58 +0530

    ui: create a logo component

---
+++ components/Logo.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Logo = () => {
+  return (
+    <h1 className="font-semibold tracking-tighter text-xl sm:text-2xl">
+      MeetLume
+    </h1>
+  );
+};
+
+export default Logo;
