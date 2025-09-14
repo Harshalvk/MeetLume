093a5ab96f31fa2a2af29a903770052e85df6fae
Harshalvk <harshalvkhobragade@gmail.com>
2025-09-14 21:32:23 +0530

    feat: add fonts.ts file to manage fonts from next/fonts

---
+++ lib/fonts.ts
@@ -0,0 +1,8 @@
+import { Spectral } from "next/font/google";
+
+export const spectral = Spectral({
+  variable: "--font-spectral",
+  subsets: ["latin"],
+  weight: ["200"],
+  style: "italic",
+});
