35886bed69d7ea545eac637f2f0b93a154dbe7d6
Harshalvk <harshalvkhobragade@gmail.com>
2025-09-16 01:44:09 +0530

    fix: update name for action

--- app/actions/user/getUserSession.ts
+++ app/actions/user/getUserSession.ts
@@ -4,7 +4,7 @@ import { auth } from "@/lib/auth";
 import { headers } from "next/headers";
 import { redirect } from "next/navigation";
 
-export default async function GetUser() {
+export default async function GetUserSession() {
   const session = await auth.api.getSession({
     headers: await headers(),
   });
