2d623b1ddc7824e48ebfd8931a9e2b80aa0fc166
Harshalvk <harshalvkhobragade@gmail.com>
2025-09-18 01:15:16 +0530

    feat: app google oauth login

--- components/landing/Navbar.tsx
+++ components/landing/Navbar.tsx
@@ -1,13 +1,25 @@
+"use client";
+
 import React from "react";
 import Logo from "../Logo";
 import { Button } from "../ui/button";
+import { signIn } from "@/lib/auth-client";
 
 const Navbar = () => {
   return (
     <nav className="w-full border rounded-xl p-2 bg-primary/2 flex justify-between">
       <Logo />
       <div>
-        <Button className="rounded-lg" size={"sm"} variant={"secondary"}>
+        <Button
+          onClick={() => {
+            signIn.social({
+              provider: "google",
+            });
+          }}
+          className="rounded-lg"
+          size={"sm"}
+          variant={"secondary"}
+        >
           Login
         </Button>
       </div>
