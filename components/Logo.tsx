e0841a8b6f9650d85da93d55ec1317f90376f2fe
Harshalvk <harshalvkhobragade@gmail.com>
2025-09-17 17:54:09 +0530

    feat: integrate spectral font into Logo component

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -1,8 +1,11 @@
+import { spectral } from "@/lib/fonts";
 import React from "react";
 
 const Logo = () => {
   return (
-    <h1 className="font-semibold tracking-tighter text-xl sm:text-2xl">
+    <h1
+      className={`font-semibold tracking-tighter text-xl sm:text-2xl ${spectral.className}`}
+    >
       MeetLume
     </h1>
   );
