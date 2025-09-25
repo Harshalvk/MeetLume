211bfd8cacda5ba327522db011242bb5954a022f
Harshalvk <harshalvkhobragade@gmail.com>
2025-09-26 01:38:53 +0530

    feat(ui): fix layout for application fix upper section on the top of page

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -3,11 +3,11 @@ import React from "react";
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className="h-screen w-full flex flex-col">
+    <div className="h-screen w-full flex flex-col overflow-clip">
       <div className="px-1 py-2 border-b border-muted-foreground/10 bg-sidebar">
         <SidebarTrigger />
       </div>
-      <div className="flex-1">{children}</div>
+      <div className="flex-1 overflow-y-auto">{children}</div>
     </div>
   );
 };
