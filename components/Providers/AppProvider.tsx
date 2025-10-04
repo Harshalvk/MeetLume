501d834ff5a33a4ac39df1498d26adb7203f5bda
Harshalvk <harshalvkhobragade@gmail.com>
2025-10-04 12:49:03 +0530

    feat: implement sonner

--- components/Providers/AppProvider.tsx
+++ components/Providers/AppProvider.tsx
@@ -4,6 +4,7 @@ import React, { useState } from "react";
 import ThemeProvider from "./ThemeProvider";
 import { UsageProvider } from "@/app/contexts/UsageContext";
 import ConditionalLayout from "../ConditionalLayout";
+import { Toaster } from "sonner";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 const AppProvider = ({ children }: { children: React.ReactNode }) => {
@@ -18,6 +19,7 @@ const AppProvider = ({ children }: { children: React.ReactNode }) => {
           enableSystem
           disableTransitionOnChange
         >
+          <Toaster />
           <UsageProvider>
             <ConditionalLayout>{children}</ConditionalLayout>
           </UsageProvider>
