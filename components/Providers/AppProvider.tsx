5bdd225d6f5ebc229dfc42bab64c71f4171cd34b
Harshalvk <harshalvkhobragade@gmail.com>
2025-10-04 12:48:30 +0530

    feat: install & setup react-query

--- components/Providers/AppProvider.tsx
+++ components/Providers/AppProvider.tsx
@@ -1,21 +1,28 @@
-import React from "react";
+"use client";
+
+import React, { useState } from "react";
 import ThemeProvider from "./ThemeProvider";
 import { UsageProvider } from "@/app/contexts/UsageContext";
 import ConditionalLayout from "../ConditionalLayout";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 const AppProvider = ({ children }: { children: React.ReactNode }) => {
+  const [queryClient] = useState(() => new QueryClient());
+
   return (
     <>
-      <ThemeProvider
-        attribute={"class"}
-        defaultTheme="system"
-        enableSystem
-        disableTransitionOnChange
-      >
-        <UsageProvider>
-          <ConditionalLayout>{children}</ConditionalLayout>
-        </UsageProvider>
-      </ThemeProvider>
+      <QueryClientProvider client={queryClient}>
+        <ThemeProvider
+          attribute={"class"}
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          <UsageProvider>
+            <ConditionalLayout>{children}</ConditionalLayout>
+          </UsageProvider>
+        </ThemeProvider>
+      </QueryClientProvider>
     </>
   );
 };
