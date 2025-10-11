2d038c087253669644f356dc4ed3898676319c66
Harshalvk <harshalvkhobragade@gmail.com>
2025-10-11 16:22:33 +0530

    fix: update base url path

--- middleware.tsx
+++ middleware.tsx
@@ -15,7 +15,7 @@ export async function middleware(request: NextRequest) {
   const { data: session } = await betterFetch<Session>(
     "/api/auth/get-session",
     {
-      baseURL: request.nextUrl.origin,
+      baseURL: process.env.BETTER_AUTH_URL,
       headers: {
         cookie: request.headers.get("cookie") || "", // Forward the cookies from the request
       },
