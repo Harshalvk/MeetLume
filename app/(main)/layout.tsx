e0bb0e7b5e6741a99cee068b3844c57d88e289e7
Harshalvk <harshalvkhobragade@gmail.com>
2025-10-12 14:30:54 +0530

    fix(ui): wrap with condiationLayout for application routes

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -1,14 +1,28 @@
+"use client";
+
+import ConditionalLayout from "@/components/ConditionalLayout";
 import { SidebarTrigger } from "@/components/ui/sidebar";
+import { usePathname } from "next/navigation";
 import React from "react";
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
+  const pathname = usePathname();
+  const actualPath =
+    pathname.split("/")[1].charAt(0).toUpperCase() + pathname.slice(2);
+
   return (
-    <div className="h-screen w-full flex flex-col overflow-clip">
-      <div className="px-1 py-2 border-b border-muted-foreground/10 bg-sidebar">
-        <SidebarTrigger />
+    <ConditionalLayout>
+      <div className="h-screen w-full flex flex-col overflow-clip">
+        <div className="px-1 py-2 border-b border-muted-foreground/10 bg-sidebar flex gap-1 items-center">
+          <SidebarTrigger />
+          <div className="h-full border-r border-border mr-2" />
+          <div>
+            <p className="text-xl font-semibold">{actualPath}</p>
+          </div>
+        </div>
+        <div className="flex-1 overflow-y-auto">{children}</div>
       </div>
-      <div className="flex-1 overflow-y-auto">{children}</div>
-    </div>
+    </ConditionalLayout>
   );
 };
 
