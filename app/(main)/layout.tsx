d5a24f4705c917d55a37ac84a56b082ee5252baf
Harshalvk <harshalvkhobragade@gmail.com>
2025-10-13 16:34:54 +0530

    fix(ui): add conditional rendering (main) route group

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -12,16 +12,22 @@ const Layout = ({ children }: { children: React.ReactNode }) => {
 
   return (
     <ConditionalLayout>
-      <div className="h-screen w-full flex flex-col overflow-clip">
-        <div className="px-1 py-2 border-b border-muted-foreground/10 bg-sidebar flex gap-1 items-center">
-          <SidebarTrigger />
-          <div className="h-full border-r border-border mr-2" />
-          <div>
-            <p className="text-xl font-semibold">{actualPath}</p>
-          </div>
+      {pathname.startsWith("/meeting") ? (
+        <div className="h-screen w-full flex flex-col overflow-clip">
+          <div className="flex-1 overflow-y-auto">{children}</div>
         </div>
-        <div className="flex-1 overflow-y-auto">{children}</div>
-      </div>
+      ) : (
+        <div className="h-screen w-full flex flex-col overflow-clip">
+          <div className="px-1 py-2 border-b border-muted-foreground/10 bg-sidebar flex gap-1 items-center">
+            <SidebarTrigger />
+            <div className="h-full border-r border-border mr-2" />
+            <div>
+              <p className="text-xl font-semibold">{actualPath}</p>
+            </div>
+          </div>
+          <div className="flex-1 overflow-y-auto">{children}</div>
+        </div>
+      )}
     </ConditionalLayout>
   );
 };
