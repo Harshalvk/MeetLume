641e7f17cbc5b8b9e26a7a62f0e17d4336f68902
Harshalvk <harshalvkhobragade@gmail.com>
2025-11-22 18:16:59 +0530

    changes: feat(fonts): add Instrument Serif font and its italic variant feat(assets): add audio and background images for landing page

--- lib/fonts.ts
+++ lib/fonts.ts
@@ -1,8 +1,21 @@
-import { Spectral } from "next/font/google";
+import { Spectral, Instrument_Serif } from "next/font/google";
 
 export const spectral = Spectral({
   variable: "--font-spectral",
   subsets: ["latin"],
   weight: ["200"],
+  style: "italic",
+});
+
+export const instrumentSerif = Instrument_Serif({
+  variable: "--font-instrument-serif",
+  subsets: ["latin"],
+  weight: ["400"],
+});
+
+export const instrumentSerifItalic = Instrument_Serif({
+  variable: "--font-instrument-serif-italic",
+  subsets: ["latin"],
+  weight: "400",
   style: "italic",
 });
